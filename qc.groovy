55c48e991857a254689b497041c8a1403309e043
Luke Zappia <lazappi@users.noreply.github.com>
2016-08-01 15:24:50 +1000

    Add fastq_screen stage

--- qc.groovy
+++ qc.groovy
@@ -30,3 +30,36 @@ fastqc = {
     }
 }
 
+fastq_screen = {
+    doc "Screening of fastq files using fastq_screen"
+
+    requires FQS_DIR   : "Directory for fastq_screen output"
+    requires NTHREADS  : "Number of threads to use"
+    requires FASTQ_DIR : "Directory containing FASTQ files"
+
+    def shortPath = {path ->
+        filename = path.split("/")[-1]
+        "$FASTQ_DIR/$filename"
+    }
+
+    def files = "${inputs}".split(" ")
+    files = files.collect{file -> shortPath(file)}
+    files = files.join(" ")
+
+    output.dir = FQS_DIR
+
+    uses(threads:NTHREADS) {
+        transform(".fastq.gz") to ("_screen.txt") {
+            exec """
+                fastq_screen
+                    --aligner bowtie2
+                    --force
+                    --outdir $output.dir
+                    --subset 100000
+                    --threads $NTHREADS
+                    $files
+            """
+        }
+        forward inputs
+    }
+}
